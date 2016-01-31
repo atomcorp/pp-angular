d5526d8715c8645eb101d0da2ce00d93d69f9d34
atomcorp <thomasmaxwellsmith@gmail.com>
2016-01-31 23:30:49 +0000

    confused

--- app/app.js
+++ app/app.js
@@ -1,28 +1,68 @@
 var app = angular.module('app', []);
 
-app.controller('homeController', ['$http', '$q', 'getFixtures', function($http, $q, getFixtures) {
+app.controller('homeController', ['$scope', '$http', '$q', '$timeout', 'getFixtures', 'displayFixtures', function ($scope, $http, $q, $timeout, getFixtures, displayFixtures) {
 	var self = this;
+
 	self.test = 'This Works';
 
 	// http://haroldrv.com/2015/02/understanding-angularjs-q-service-and-promises/
-	getFixtures.getResponse()
-		.then( function(result) {
-			self.fixtures = result.data.data[0];
-			console.log(self.fixtures);
-		});
-	
+
+	var deferred = $q.defer();
+
+	this.fixtures = function() {
+		displayFixtures.createFixtures()
+			.then( function (result) {
+				console.log(displayFixtures.data);
+				return result;
+			});
+	}
+
+	console.log(displayFixtures.data);
+
 }]);
 
 app.service('getFixtures', ['$http', '$q', function ($http, $q) {
 	var self = this;
 
-	self.getResponse = function() {
+	self.requestFixtures = function() {
 		return $http.get('/app/example-data/fixtures.json')
 			.then(function(response) {
 				return response;
 			});
-		
 	}
-	
-	return self;
+
+}]);
+
+app.service('displayFixtures', ['getFixtures', function (getFixtures) {
+
+	var self = this;
+
+	self.createFixtures = function() {
+		getFixtures.requestFixtures()
+			.then( function (result) {
+				var data = result.data.data;
+				return data;
+				console.log(result);
+
+			}).then( function(result) {
+				result = makeFixtures(data);
+				console.log(result);
+			}).then ( function(result) {
+				self.data = result;
+				return self.data;
+			});
+	}
+
+	function makeFixtures(data) {
+		list = [];
+		for (var i = 0; i < data.length; i++) {
+			var fixture = {};
+			fixture.homeTeam = data[i].home;
+			fixture.awayTeam = data[i].away;
+			fixture.data = data[i].date;
+			list.push(fixture);
+		};
+		return list;
+	}	
+
 }]);
